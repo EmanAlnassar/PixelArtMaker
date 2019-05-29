4ecf97906c42a77c7c65fdd8f3def78d78198c51
EmanAlnassar <eman.alnassar@gmail.com>
2019-05-30 01:15:00 +0300

    feat: create make grid function

--- designs.js
+++ designs.js
@@ -4,7 +4,14 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+    const table = document.getElementById('pixelCanvas');
+    const hight = document.getElementById('inputHeight').value;
+    const width = document.getElementById('inputWidth').value;
+    for (let i = 0; i < hight; i++) {
+        const row = table.insertRow(i);
+        for (let x = 0; x < width; x++) {
+            row.insertCell(x);
+        }
+    }
+};
 
-// Your code goes here!
-
-}
