15886b094c9630d8b3f2fef8b970d5edae17758a
EmanAlnassar <eman.alnassar@gmail.com>
2019-05-30 01:22:09 +0300

    feat: add comment to the code

--- designs.js
+++ designs.js
@@ -5,8 +5,10 @@
 
 function makeGrid() {
     const table = document.getElementById('pixelCanvas');
+     // Getting the values of hight and width from user inputs
     const hight = document.getElementById('inputHeight').value;
     const width = document.getElementById('inputWidth').value;
+    // Creating the grid which is composed of rows filled with cells
     for (let i = 0; i < hight; i++) {
         const row = table.insertRow(i);
         for (let x = 0; x < width; x++) {
@@ -15,14 +17,14 @@ function makeGrid() {
     }
 };
 
-
+// An event to respond when the user submits the form to make the grid
 const form = document.getElementById('sizePicker');
 form.addEventListener('submit', function (evt) {
     evt.preventDefault();
     makeGrid();
 });
 
-
+// An event to apply the picked color to the chosen cell via a click
 const table = document.getElementById('pixelCanvas');
 table.addEventListener('click', function (evt) {
     if (evt.target.nodeName.toLowerCase() === 'td') {
